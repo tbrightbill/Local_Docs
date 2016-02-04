154902e4b4d14f8ee5db368eb4e580c0629a0000
Dennis Gahm <dennisg0505@gmail.com>
2016-02-04 10:28:39 -0800

    test commit

--- app/src/main/java/csm117/localdocs/EditorActivity.java
+++ app/src/main/java/csm117/localdocs/EditorActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class EditorActivity extends AppCompatActivity {
-
+	//test comment
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
