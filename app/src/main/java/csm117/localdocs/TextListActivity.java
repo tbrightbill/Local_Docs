ad8ff3fc56fa338d82e28b2151d2f993002d9f27
Jaehyung Park <neojinkazu@gmail.com>
2016-02-04 15:08:54 -0800

    Page to show list of texts

--- app/src/main/java/csm117/localdocs/TextListActivity.java
+++ app/src/main/java/csm117/localdocs/TextListActivity.java
@@ -4,5 +4,42 @@ package csm117.localdocs;
  * Created by Jae on 2/4/2016.
  */
 
-public class TextListActivity {
+public class TextListActivity extends activity{
+    /*
+    get files in storage
+     */
+    public ArrayList<String> GetFiles(String DirectoryPath){
+        ArrayList<String> MyFiles = new ArrayList<String>();
+        File f = new File(DirectoryPath);
+
+        f.mkdirs();
+        File[] files = f.listFiles();
+        if (files.length == 0)
+            return null;
+        else {
+            for (int i=0; i<files.length; i++)
+                MyFiles.add(files[i].getName());
+        }
+
+        return MyFiles;
+    }
+
+    @Override
+    public void onCreate() {
+    // Other Code
+
+        ListView lv;
+        ArrayList<String> FilesInFolder = GetFiles("/sdcard/somefolder");
+        lv = (ListView)findViewById(R.id.filelist);
+
+        lv.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, FilesInFolder));
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                // Clicking on items
+            }
+        });
+    }
 }
+
